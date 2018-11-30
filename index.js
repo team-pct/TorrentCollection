2c060858d7b19f60c3c870345cd44e8ccaf9344e
Team Pct <teampct@tuta.io>
2018-11-30 13:01:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const Cpb             = require('cpasbien-api');
+const ExtraTorrentAPI = require('extratorrent-api');
+const leet            = require('xtorrent');
+const rbg             = require('torrentapi-wrapper');
+const tpb             = require('thepiratebay');
+module.exports = {
+  Cpb: Cpb,
+  ExtraTorrentAPI: ExtraTorrentAPI,
+  leet: leet,
+  rbg: rbg,
+  tpb: tpb
+};
